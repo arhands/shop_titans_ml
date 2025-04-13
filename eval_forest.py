88611d37872110b6e31675bc8f9c6a11484050e9
Andrew Hands <andrewrhands@gmail.com>
2025-04-13 18:40:59 -0500

    added additional options for input/output

--- eval_forest.py
+++ eval_forest.py
@@ -7,12 +7,27 @@ from sklearn.ensemble import RandomForestClassifier
 from image_manipulate_utils import filter_rgb_color_range, find_connected_disjoint_structures
 # process arguments
 parser = ArgumentParser()
-parser.add_argument('-d', '--directory', type=str, required=True)
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('-d', '--directory', type=str, default=None)
+group.add_argument('-i', '--input_coords_file_path', type=str, default=None,
+                   help='path to input coords file. File should be comma delimited as left, top, width, height')
+parser.add_argument('-o', '--output_value_path', default=None)
 args = parser.parse_args()
-path: str = args.directory
-
+output_value_path: str | None = args.output_value_path
+img: Image.Image
+if args.directory is not None:
+    directory: str = args.directory
+    img = Image.open(directory)
+else:
+    args_path: str = args.input_coords_file_path
+    from pyscreeze import Box, screenshot
+    with open(args_path, 'r') as f:
+        x, y, w, h = map(int, f.read().split(','))
+    img = screenshot(
+        region=Box(x, y, w, h))
+img.save('output.png')
+image = np.array(img.convert('RGB'))
 # loading image
-image: np.ndarray = np.array(Image.open(path).convert('RGB'))
 image = filter_rgb_color_range(
     image, (160, 154, 157), (255, 255, 255))
 image = np.all(image != 0, -1)
@@ -36,4 +51,9 @@ for symbol in find_connected_disjoint_structures(image):
     number_xpos_pairs.append((y, symbol[0][1]))
     # number_xpos_pairs.append((Y_pred[0], symbol[0][0]))
 number_xpos_pairs.sort(key=lambda p: p[1])
-print(''.join([str(p[0]) for p in number_xpos_pairs]))
+res = ''.join([str(p[0]) for p in number_xpos_pairs])
+if output_value_path is not None:
+    with open(output_value_path, 'w') as f:
+        f.write(res)
+else:
+    print(res)
